b304b1038d00c46ba2d9289d3fb1fa147e1723ec
hooni <jhlee909090@gmail.com>
2025-04-25 11:05:59 +0900

    add: xcode init

---
+++ NoWeekend/NoWeekend/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoWeekend
+//
+//  Created by 이지훈 on 4/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoWeekend/NoWeekend/NoWeekendApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoWeekendApp.swift
+//  NoWeekend
+//
+//  Created by 이지훈 on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct NoWeekendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
