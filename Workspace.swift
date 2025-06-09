2c24018a80d468ab3ac16e4314d14267e678e39b
hooni <jhlee909090@gmail.com>
2025-06-10 00:15:28 +0900

    feat/#11 - workSpace 정의

--- Workspace.swift
+++ Workspace.swift
@@ -3,10 +3,19 @@ import ProjectDescription
 let workspace = Workspace(
     name: "NoWeekend",
     projects: [
+        // Application Layer
         "Projects/App",
+        
+        // Features Layer
+        "Projects/Features",
+        
+        // UI Layer
+        "Projects/UI",
+        
+        // Core Layer
         "Projects/Core",
-        "Projects/Domain",
-        "Projects/Data",
-        "Projects/Presentation",
+        
+        // Presentation
+        "Projects/Presentation"
     ]
 )
