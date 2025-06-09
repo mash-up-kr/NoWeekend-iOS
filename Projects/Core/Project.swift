28cffe1e17774b60bd5019603c78a9a25e1e8add
hooni <jhlee909090@gmail.com>
2025-06-10 00:35:18 +0900

    feat: #11 코어 레이어 분리

--- Projects/Core/Project.swift
+++ Projects/Core/Project.swift
@@ -3,17 +3,82 @@ import ProjectDescription
 let project = Project(
     name: "Core",
     targets: [
+        // Common 타겟
         .target(
-            name: "Core",
+            name: "Common",
             destinations: .iOS,
             product: .framework,
-            bundleId: "com.noweekend.core",
+            bundleId: "com.noweekend.common",
             deploymentTargets: .iOS("17.0"),
             infoPlist: .default,
-            sources: ["Sources/**"],
-            resources: [],
+            sources: ["Common/Sources/**"],
+            dependencies: []
+        ),
+        // Util 타겟
+        .target(
+            name: "Util",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "com.noweekend.util",
+            deploymentTargets: .iOS("17.0"),
+            infoPlist: .default,
+            sources: ["Util/Sources/**"],
             dependencies: [
-
+                .target(name: "Common")
+            ]
+        ),
+        // Domain 타겟
+        .target(
+            name: "Domain",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "com.noweekend.domain",
+            deploymentTargets: .iOS("17.0"),
+            infoPlist: .default,
+            sources: ["Domain/Sources/**"],
+            dependencies: []
+        ),
+        // Network 타겟
+        .target(
+            name: "Network",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "com.noweekend.network",
+            deploymentTargets: .iOS("17.0"),
+            infoPlist: .default,
+            sources: ["Network/Sources/**"],
+            dependencies: [
+                .target(name: "Common"),
+                .external(name: "Alamofire")
+            ]
+        ),
+        // Storage 타겟
+        .target(
+            name: "Storage",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "com.noweekend.storage",
+            deploymentTargets: .iOS("17.0"),
+            infoPlist: .default,
+            sources: ["Storage/Sources/**"],
+            dependencies: [
+                .target(name: "Common")
+            ]
+        ),
+        // Data 타겟
+        .target(
+            name: "Data",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "com.noweekend.data",
+            deploymentTargets: .iOS("17.0"),
+            infoPlist: .default,
+            sources: ["Data/Sources/**"],
+            dependencies: [
+                .target(name: "Network"),
+                .target(name: "Storage"),
+                .target(name: "Domain"),
+                .target(name: "Util")
             ]
         )
     ]
