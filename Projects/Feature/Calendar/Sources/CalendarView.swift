5f9423994bd564b460f8c8b8de45a5357deae1c8
hooni <jhlee909090@gmail.com>
2025-06-11 01:36:00 +0900

    fix: # 11 - 캘린터 뷰

---
+++ Projects/Feature/Calendar/Sources/CalendarView.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+import CalendarInterface
+import Domain
+import DesignSystem
+import Common
+
+public struct CalendarView: View {
+    public init() {}
+    
+    public var body: some View {
+        VStack {
+            Text("지훈")
+                .font(Typography.title)
+        }
+    }
+}
+
+#Preview {
+    CalendarView()
+}
