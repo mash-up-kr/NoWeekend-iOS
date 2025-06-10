cb6b733bec18ad5d1937c09610dd8f9f0969e1f1
hooni <jhlee909090@gmail.com>
2025-06-11 01:35:39 +0900

    fix: # 11 - 탭바 레이어 조립

--- Projects/Feature/TabBar/Sources/TabBarView.swift
+++ Projects/Feature/TabBar/Sources/TabBarView.swift
@@ -1,31 +1,39 @@
 import SwiftUI
 import Home
-import Calendar
 import Profile
+import Calendar
+import DesignSystem
+import Common
 
 public struct TabBarView: View {
+    @State private var selectedTab = 0
+    
     public init() {}
     
     public var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             HomeView()
                 .tabItem {
-                    Image(systemName: "house")
+                    Image(systemName: "house.fill")
                     Text("Home")
                 }
+                .tag(0)
             
             CalendarView()
                 .tabItem {
                     Image(systemName: "calendar")
                     Text("Calendar")
                 }
+                .tag(1)
             
             ProfileView()
                 .tabItem {
-                    Image(systemName: "person")
+                    Image(systemName: "person.fill")
                     Text("Profile")
                 }
+                .tag(2)
         }
+        .accentColor(Colors.primary)
     }
 }
 
