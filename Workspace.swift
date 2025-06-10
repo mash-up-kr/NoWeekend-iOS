2ce901b438efc7f3bd0d18f01e8bf10f9e88f615
hooni <jhlee909090@gmail.com>
2025-06-11 01:24:18 +0900

    fix: #11 - Updates project structure

--- Workspace.swift
+++ Workspace.swift
@@ -3,19 +3,11 @@ import ProjectDescription
 let workspace = Workspace(
     name: "NoWeekend",
     projects: [
-        // Application Layer
         "Projects/App",
-        
-        // Features Layer
-        "Projects/Features",
-        
-        // UI Layer
-        "Projects/UI",
-        
-        // Core Layer
+        "Projects/Feature", 
+        "Projects/Interface",
         "Projects/Core",
-        
-        // Presentation
-        "Projects/Presentation"
+        "Projects/Shared",
+        "Projects/Plugin"
     ]
 )
