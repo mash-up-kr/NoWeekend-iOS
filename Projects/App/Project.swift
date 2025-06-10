49a880b750f38ec40e6ea8e8953f3e5fdc06f48f
hooni <jhlee909090@gmail.com>
2025-06-10 22:05:26 +0900

    fix: #11 - SignIn 삭제한거 되돌리기

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -9,20 +9,53 @@ let project = Project(
             product: .app,
             bundleId: "com.noweekend.app",
             deploymentTargets: .iOS("17.0"),
-            infoPlist: .extendingDefault(
-                with: [
-                    "UILaunchScreen": [
-                        "UIColorName": "",
-                        "UIImageName": "",
-                    ],
-                ]
-            ),
+            infoPlist: .file(path: .relativeToRoot("Projects/App/Info.plist")),
             sources: ["Sources/**"],
+            resources: [
+                "Resources/**"
+            ],
             dependencies: [
-                .project(target: "OnBoarding", path: "../Features"),
-                .project(target: "TabBar", path: "../Features")
-                // Data 의존성 제거!
-            ]
+                .project(target: "Presentation", path: "../Presentation"),
+                .project(target: "Common", path: "../Core"),
+                .project(target: "Data", path: "../Core"),
+                .project(target: "Domain", path: "../Core"),
+                .project(target: "TabBar", path: "../Features"),
+                .project(target: "DesignSystem", path: "../UI"),
+            ],
+            settings: .settings(
+                base: [
+                    "CODE_SIGN_STYLE": "Manual",
+                    "DEVELOPMENT_TEAM": "SQ5T25W9V5"
+                ],
+                configurations: [
+                    .debug(
+                        name: .debug,
+                        settings: [
+                            "PROVISIONING_PROFILE_SPECIFIER": "match Development com.noweekend.app",
+                            "CODE_SIGN_IDENTITY": "Apple Development"
+                        ],
+                        xcconfig: nil
+                    ),
+                    .release(
+                        name: .release,
+                        settings: [
+                            "PROVISIONING_PROFILE_SPECIFIER": "match AppStore com.noweekend.app",
+                            "CODE_SIGN_IDENTITY": "Apple Distribution"
+                        ],
+                        xcconfig: nil
+                    )
+                ]
+            )
+        )
+    ],
+    schemes: [
+        .scheme(
+            name: "App",
+            shared: true,
+            buildAction: .buildAction(targets: ["App"]),
+            testAction: TestAction.targets([]),
+            runAction: .runAction(configuration: .debug),
+            archiveAction: .archiveAction(configuration: .release)
         )
     ]
 )
