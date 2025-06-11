8dcd2453ac9b3765ee610492945650e109567e24
SijongKim93 <rlatlwhd456@gmail.com>
2025-06-11 15:08:59 +0900

    feat/#14-Core(구현체)구현- AuthRepositoryImpl, GoogleLoginUseCase

--- Projects/Core/UseCase/Sources/Auth/LoginWithGoogleUseCase.swift
+++ Projects/Core/UseCase/Sources/Auth/LoginWithGoogleUseCase.swift
@@ -6,4 +6,18 @@
 //
 
 import Foundation
+import Domain
+import RepositoryInterface
+import LoginInterface
 
+public final class LoginWithGoogleUseCase: LoginWithGoogleUseCaseInterface {
+    private let authRepository: AuthRepositoryInterface
+    
+    public init(authRepository: AuthRepositoryInterface) {
+        self.authRepository = authRepository
+    }
+    
+    public func execute(accessToken: String, name: String?) async throws -> LoginUser {
+        return try await authRepository.loginWithGoogle(accessToken: accessToken, name: name)
+    }
+}
