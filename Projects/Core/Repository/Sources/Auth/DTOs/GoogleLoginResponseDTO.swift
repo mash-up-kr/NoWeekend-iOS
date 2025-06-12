abc9f7f261045ae59dc55f8ecfe1d13498ba296b
SijongKim93 <rlatlwhd456@gmail.com>
2025-06-12 17:26:21 +0900

    fix/#14-pull conflict 해결(2)

--- Projects/Core/Repository/Sources/Auth/DTOs/GoogleLoginResponseDTO.swift
+++ Projects/Core/Repository/Sources/Auth/DTOs/GoogleLoginResponseDTO.swift
@@ -29,10 +29,6 @@ extension GoogleLoginResponseDTO {
     func toDomain() -> LoginUser {
         return LoginUser(
             email: self.email,
-<<<<<<< HEAD
-=======
-            name: nil,
->>>>>>> 2948d342ca4a17699338a4d2de2f4bfe1a2b77a1
             accessToken: self.accessToken,
             isExistingUser: self.exists
         )
--- Projects/Interface/Domain/Sources/Login/LoginUser.swift
+++ Projects/Interface/Domain/Sources/Login/LoginUser.swift
@@ -11,7 +11,6 @@ public struct LoginUser {
     public let email: String
     public let accessToken: String?
     public let isExistingUser: Bool?
-<<<<<<< HEAD
     public let isOnboardingCompleted: Bool
     
     public init(email: String, accessToken: String?, isExistingUser: Bool?, isOnboardingCompleted: Bool = false) {
@@ -19,12 +18,5 @@ public struct LoginUser {
         self.accessToken = accessToken
         self.isExistingUser = isExistingUser
         self.isOnboardingCompleted = isOnboardingCompleted
-=======
-    
-    public init(email: String, accessToken: String?, isExistingUser: Bool?) {
-        self.email = email
-        self.accessToken = accessToken
-        self.isExistingUser = isExistingUser
->>>>>>> 2948d342ca4a17699338a4d2de2f4bfe1a2b77a1
     }
 }
