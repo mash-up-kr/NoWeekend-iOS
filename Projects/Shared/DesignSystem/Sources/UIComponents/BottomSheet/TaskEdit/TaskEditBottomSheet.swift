0a5f42123c0d48740be51bc38adfb4b54e105e0f
hooni <jhlee909090@gmail.com>
2025-06-18 16:17:54 +0900

    Style/#34 - 할일수정 삭제 2개만 있는 코드 구현

---
+++ Projects/Shared/DesignSystem/Sources/UIComponents/BottomSheet/TaskEdit/TaskEditBottomSheet.swift
@@ -0,0 +1,79 @@
+//
+//  TaskEditBottomSheet.swift
+//  Shared
+//
+//  Created by 이지훈 on 6/18/25.
+//  Copyright © 2025 com.noweekend. All rights reserved.
+//
+
+import SwiftUI
+
+/// 할 일 수정 바텀시트
+struct TaskEditBottomSheet: View {
+    let onEditAction: () -> Void
+    let onTomorrowAction: () -> Void
+    let onDeleteAction: () -> Void
+    @Binding var isPresented: Bool
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            CustomDragIndicator()
+            
+            VStack(spacing: 0) {
+                Button(action: {
+                    onEditAction()
+                    isPresented = false
+                }) {
+                    HStack(spacing: 8) {
+                        Image(.icnEdit)
+                        
+                        Text(" 할 일 수정")
+                            .font(.body1)
+                            .foregroundColor(DS.Colors.Text.gray900)
+                        
+                        Spacer()
+                    }
+                    .frame(height: 56)
+                }
+                
+                Button(action: {
+                    onTomorrowAction()
+                    isPresented = false
+                }) {
+                    HStack(spacing: 8) {
+                        Image(.icnArrowForward)
+                        
+                        Text("내일 또 하기")
+                            .font(.body1)
+                            .foregroundColor(DS.Colors.Text.gray900)
+                        
+                        Spacer()
+                    }
+                    .frame(height: 56)
+                }
+                
+                Button(action: {
+                    onDeleteAction()
+                    isPresented = false
+                }) {
+                    HStack(spacing: 8) {
+                        Image(.icnDelete)
+                        
+                        Text("삭제하기")
+                            .font(.body1)
+                            .foregroundColor(DS.Colors.Text.gray900)
+                        
+                        Spacer()
+                    }
+                    .frame(height: 56)
+                }
+            }
+            .padding(.horizontal, 20)
+            
+            Spacer()
+        }
+        .presentationDetents([.height(192)])
+        .presentationDragIndicator(.hidden)
+        .presentationCornerRadius(16)
+    }
+}
