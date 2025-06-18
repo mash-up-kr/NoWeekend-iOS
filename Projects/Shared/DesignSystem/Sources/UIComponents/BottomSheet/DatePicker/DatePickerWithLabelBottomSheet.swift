cc3c2156b11820e6c41d66488b763653a20b6414
hooni <jhlee909090@gmail.com>
2025-06-18 16:17:28 +0900

    Style/#34 - 라벨있는 데이트 피커 구현

---
+++ Projects/Shared/DesignSystem/Sources/UIComponents/BottomSheet/DatePicker/DatePickerWithLabelBottomSheet.swift
@@ -0,0 +1,55 @@
+//
+//  DatePickerBottomSheets.swift
+//  Shared
+//
+//  Created by 이지훈 on 6/18/25.
+//  Copyright © 2025 com.noweekend. All rights reserved.
+//
+
+import SwiftUI
+
+/// 레이블이 있는 날짜 선택 바텀시트
+struct DatePickerWithLabelBottomSheet: View {
+    @Binding var selectedDate: Date
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            CustomDragIndicator()
+            
+            VStack(spacing: 20) {
+                VStack(spacing: 4) {
+                    Text("확인하고 싶은")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                    Text("휴가 날짜를 선택해주세요")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+                .multilineTextAlignment(.center)
+                
+                DatePickerBottomSheet(selectedDate: $selectedDate)
+            }
+            .padding(.horizontal, 20)
+        }
+        .presentationDetents([.height(400)])
+        .presentationDragIndicator(.hidden)
+        .presentationCornerRadius(16)
+    }
+}
+
+/// 레이블이 없는 날짜 선택 바텀시트
+struct DatePickerOnlyBottomSheet: View {
+    @Binding var selectedDate: Date
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            CustomDragIndicator()
+            
+            DatePickerBottomSheet(selectedDate: $selectedDate)
+                .padding(.horizontal, 20)
+        }
+        .presentationDetents([.height(300)])
+        .presentationDragIndicator(.hidden)
+        .presentationCornerRadius(16)
+    }
+}
