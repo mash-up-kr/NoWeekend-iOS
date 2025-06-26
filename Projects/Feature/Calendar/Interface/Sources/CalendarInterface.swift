901888f099a788712c98d914a3ed24d2de51202f
hooni <jhlee909090@gmail.com>
2025-06-26 15:24:26 +0900

    fix/#38 - 캘린더 인터페이스 depandency 삭제

--- Projects/Feature/Calendar/Interface/Sources/CalendarInterface.swift
+++ Projects/Feature/Calendar/Interface/Sources/CalendarInterface.swift
@@ -1,12 +1,6 @@
 import Foundation
 import SwiftUI
-import NeedleFoundation
 import Entity
-
-// MARK: - Calendar Dependency Protocol
-public protocol CalendarDependency: Dependency {
-    // var eventUseCase: EventUseCase { get }
-}
 
 // MARK: - Calendar Builder Protocol
 public protocol CalendarBuilder {
