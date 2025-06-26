9269cfb5f4e73fbe437e8be0b1d6be3e6c112421
hooni <jhlee909090@gmail.com>
2025-06-26 15:24:19 +0900

    fix/#38 - 온보딩 인터페이스 depandency 삭제

--- Projects/Feature/Onboarding/Interface/Sources/OnboardingInterface.swift
+++ Projects/Feature/Onboarding/Interface/Sources/OnboardingInterface.swift
@@ -1,10 +1,5 @@
 import Foundation
 import SwiftUI
-import NeedleFoundation
-
-// MARK: - Onboarding Dependency Protocol  
-public protocol OnboardingDependency: Dependency {
-}
 
 // MARK: - Onboarding Builder Protocol
 public protocol OnboardingBuilder {
