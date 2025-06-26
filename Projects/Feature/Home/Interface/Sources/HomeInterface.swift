93d8c7d88fb0b362edaaa5c6aeeeb3d6464467f8
hooni <jhlee909090@gmail.com>
2025-06-26 15:24:12 +0900

    fix/#38 - 홈 인터페이스 depandency 삭제

--- Projects/Feature/Home/Interface/Sources/HomeInterface.swift
+++ Projects/Feature/Home/Interface/Sources/HomeInterface.swift
@@ -1,13 +1,7 @@
 import Foundation
 import SwiftUI
-import NeedleFoundation
 import Entity
 
-// MARK: - Home Dependency Protocol
-public protocol HomeDependency: Dependency {
-    // var userUseCase: UserUseCase { get }
-    // var eventUseCase: EventUseCase { get }
-}
 
 // MARK: - Home Builder Protocol
 public protocol HomeBuilder {
