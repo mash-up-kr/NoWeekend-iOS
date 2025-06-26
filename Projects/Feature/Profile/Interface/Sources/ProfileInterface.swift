035fc68698f06c4be668fcdac35b3be505c09c8d
hooni <jhlee909090@gmail.com>
2025-06-26 15:24:53 +0900

    fix/#38 - 프로필 인터페이스 depandency 삭제

--- Projects/Feature/Profile/Interface/Sources/ProfileInterface.swift
+++ Projects/Feature/Profile/Interface/Sources/ProfileInterface.swift
@@ -1,12 +1,6 @@
 import Foundation
 import SwiftUI
-import NeedleFoundation
 import Entity
-
-// MARK: - Profile Dependency Protocol
-public protocol ProfileDependency: Dependency {
-    // var userUseCase: UserUseCase { get }
-}
 
 // MARK: - Profile Builder Protocol
 public protocol ProfileBuilder {
