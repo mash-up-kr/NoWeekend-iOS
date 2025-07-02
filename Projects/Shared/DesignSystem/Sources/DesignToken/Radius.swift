409b8cef44f88e36662290d95b23b943209d39e6
k-nh <skgml977@gmail.com>
2025-07-03 02:18:50 +0900

    feat/#47-디자인 토큰 및 이미지 추가

---
+++ Projects/Shared/DesignSystem/Sources/DesignToken/Radius.swift
@@ -0,0 +1,21 @@
+//
+//  Radius.swift
+//  DesignSystem
+//
+//  Created by 김나희 on 7/3/25.
+//  Copyright © 2025 com.noweekend. All rights reserved.
+//
+
+import Foundation
+
+enum Radius {
+    static let radius000: CGFloat = 0
+    static let radius050: CGFloat = 2
+    static let radius100: CGFloat = 4
+    static let radius200: CGFloat = 6
+    static let radius300: CGFloat = 8
+    static let radius400: CGFloat = 12
+    static let radius500: CGFloat = 16
+    static let radius600: CGFloat = 20
+    static let radius700: CGFloat = 24
+}
---
+++ Projects/Shared/DesignSystem/Sources/DesignToken/Spacing.swift
@@ -0,0 +1,27 @@
+//
+//  Spacing.swift
+//  DesignSystem
+//
+//  Created by 김나희 on 7/3/25.
+//  Copyright © 2025 com.noweekend. All rights reserved.
+//
+
+import Foundation
+
+enum Spacing {
+    static let space000: CGFloat = 0
+    static let space025: CGFloat = 2
+    static let space050: CGFloat = 4
+    static let space075: CGFloat = 6
+    static let space100: CGFloat = 8
+    static let space150: CGFloat = 12
+    static let space175: CGFloat = 16
+    static let space200: CGFloat = 20
+    static let space300: CGFloat = 24
+    static let space400: CGFloat = 32
+    static let space500: CGFloat = 40
+    static let space600: CGFloat = 48
+    static let space700: CGFloat = 56
+    static let space800: CGFloat = 64
+    static let space900: CGFloat = 72
+}
