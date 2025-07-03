083c8d2bd124dd3b72672bd64c2ed370323cdceb
hooni <jhlee909090@gmail.com>
2025-07-04 02:22:24 +0900

    feat/#38 CalendarFeatureAssembly 캘린더에서 등록

---
+++ Projects/Calendar/CalendarFeature/Sources/Assembly/CalendarFeatureAssembly.swift
@@ -0,0 +1,29 @@
+//
+//  CalendarFeatureAssembly.swift
+//  CalendarFeature
+//
+//  Created by 이지훈 on 7/3/25.
+//  Copyright © 2025 com.noweekend. All rights reserved.
+//
+
+import Foundation
+import Swinject
+import CalendarDomain
+import Core
+
+public struct CalendarFeatureAssembly: Assembly {
+    public init() {}
+    
+    public func assemble(container: Container) {
+        print("📅 CalendarFeatureAssembly 등록 시작")
+        
+        // UseCase만 등록 
+        container.register(CalendarUseCaseProtocol.self) { resolver in
+            print("📋 CalendarUseCase 생성 (Feature)")
+            let repository = resolver.resolve(CalendarRepositoryProtocol.self)!
+            return CalendarUseCase(calendarRepository: repository)
+        }.inObjectScope(.graph)
+        
+        print("✅ CalendarFeatureAssembly 등록 완료")
+    }
+}
