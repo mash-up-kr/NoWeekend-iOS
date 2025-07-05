1c28a2cb6cc9dc87dd0f54afc715eddb0f19db14
hooni <jhlee909090@gmail.com>
2025-07-06 00:14:08 +0900

    feat/#43 도메인 레이어 구조 수정

---
+++ Projects/Calendar/CalendarDomain/Sources/UseCase/CalendarUseCase.swift
@@ -0,0 +1,37 @@
+//
+//  CalendarUseCase.swift
+//  CalendarFeature
+//
+//  Created by 이지훈 on 7/3/25.
+//  Copyright © 2025 com.noweekend. All rights reserved.
+//
+
+import Foundation
+
+public class CalendarUseCase: CalendarUseCaseProtocol {
+    private let calendarRepository: CalendarRepositoryProtocol
+    
+    public init(calendarRepository: CalendarRepositoryProtocol) {
+        self.calendarRepository = calendarRepository
+    }
+    
+    public func getCalendarEvents() async throws -> [CalendarEvent] {
+        return try await calendarRepository.getCalendarEvents()
+    }
+    
+    public func getEventsForDate(_ date: Date) async throws -> [CalendarEvent] {
+        return try await calendarRepository.getEventsForDate(date)
+    }
+    
+    public func createCalendarEvent(_ event: CalendarEvent) async throws {
+        try await calendarRepository.createCalendarEvent(event)
+    }
+    
+    public func updateCalendarEvent(_ event: CalendarEvent) async throws {
+        try await calendarRepository.updateCalendarEvent(event)
+    }
+    
+    public func deleteCalendarEvent(id: String) async throws {
+        try await calendarRepository.deleteCalendarEvent(id: id)
+    }
+}
