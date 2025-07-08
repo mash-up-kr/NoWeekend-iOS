fc14da3986a3fd250215d6259e4699ec989a2302
hooni <jhlee909090@gmail.com>
2025-07-08 23:15:42 +0900

    Remove Config.swift from tracking

---
+++ Projects/Core/NWNetwork/Sources/NetworkService/Config.swift
@@ -0,0 +1,13 @@
+//
+//  Config.swift
+//  Coordinator
+//
+//  Created by 김나희 on 7/6/25.
+//  Copyright © 2025 com.noweekend. All rights reserved.
+//
+
+import Foundation
+
+public enum Config {
+    public static let baseURL = "https://noweekend.store/api/v1"
+}
