0cdb202dd118a56b1d416cfa09c9fd73fe36ce7c
hooni <jhlee909090@gmail.com>
2025-07-08 17:51:25 +0900

    feat/#60 확인버튼 넣기

--- Projects/Shared/DesignSystem/Sources/UIComponents/BottomSheet/DatePicker/DatePickerWithLabelBottomSheet.swift
+++ Projects/Shared/DesignSystem/Sources/UIComponents/BottomSheet/DatePicker/DatePickerWithLabelBottomSheet.swift
@@ -10,20 +10,30 @@ import SwiftUI
 
 public struct DatePickerWithLabelBottomSheet: View {
     @Binding public var selectedDate: Date
+    @Environment(\.dismiss) private var dismiss
     
     public init(selectedDate: Binding<Date>) {
         self._selectedDate = selectedDate
     }
     
     public var body: some View {
-        BottomSheetContainer(height: 400) {
-            VStack(spacing: 20) {
+        BottomSheetContainer(height: 480) {
+            VStack(spacing: 24) {
                 BottomSheetHeader(
                     title: "확인하고 싶은",
                     subtitle: "휴가 날짜를 선택해주세요"
                 )
                 
                 DatePickerBottomSheet(selectedDate: $selectedDate)
+                Spacer()
+                NWButton.black(
+                    "확인",
+                    size: .xl
+                ) {
+                    dismiss()
+                }
+                .padding(.horizontal, 0)
+                .padding(.bottom, 8)
             }
         }
     }
