a035f0477976231f684153d4702f01361c280330
hooni <jhlee909090@gmail.com>
2025-07-23 00:00:04 +0900

    feat/#124 테스트 피쳐 생성

---
+++ Projects/Calendar/CalendarFeature/Tests/CalendarFeatureTests.swift
@@ -0,0 +1,16 @@
+//
+//  CalendarFeatureTests.swift
+//  CalendarExampleApp
+//
+//  Created by 이지훈 on 7/22/25.
+//  Copyright © 2025 com.noweekend. All rights reserved.
+//
+
+import XCTest
+@testable import CalendarFeature
+
+final class CalendarFeatureTests: XCTestCase {
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+}
